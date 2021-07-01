7fbce908979ace9b7cffff1d000cc626267af92f
Anthony Bilodeau <anthonybilodeau214@gmail.com>
2021-07-01 10:47:26 -0400

    added bounded rewards and MO reward;

--- gym_sted/rewards/rewards.py
+++ gym_sted/rewards/rewards.py
@@ -2,8 +2,40 @@
 import numpy
 
 class RewardCalculator:
-    def __init__(self, objs, bounds):
+    def __init__(self, objs):
+        """
+        Instantiates the `RewardCalculator`
 
+        :param objs: A `dict` of objective
+        """
+        self.objectives = objs
+
+    def evaluate(self, sted_image, conf1, conf2, fg_s, fg_c):
+        """
+        Evaluates the objectives and returns whether all objectives are within
+        the predefined bounds
+
+        :param sted_stack: A `numpy.ndarray` of the acquired STED image
+        :param conf1: A `numpy.ndarray` of the acquired confocal image before
+        :param conf2: A `numpy.ndarray` of the acquired confocal image after
+        :param fg_s: A `numpy.ndarray` of the STED foreground
+        :param fg_c: A `numpy.ndarray` of the confocal foreground
+
+        :returns : A `list` of rewards
+        """
+        return [
+            self.objectives[obj_name].evaluate([sted_image], conf1, conf2, fg_s, fg_c), obj_name)
+            for obj_name in self.objectives.keys()
+        ]
+
+class BoundedRewardCalculator:
+    def __init__(self, objs, bounds):
+        """
+        Instantiates the `RewardCalculator`
+
+        :param objs: A `dict` of objective
+        :param bounds: A `dict` of the bounds for each objective
+        """
         self.objectives = objs
         self.bounds = bounds
 
@@ -17,9 +49,13 @@ class RewardCalculator:
         :param conf2: A `numpy.ndarray` of the acquired confocal image after
         :param fg_s: A `numpy.ndarray` of the STED foreground
         :param fg_c: A `numpy.ndarray` of the confocal foreground
+
+        :returns : An `int` in  `{0, 1}` if all objectives are within bounds
         """
-        return int(all([self.isin(self.objectives[obj_name].evaluate([sted_image], conf1, conf2, fg_s, fg_c), obj_name)
-                    for obj_name in self.objectives.keys()]))
+        return int(all([
+            self.isin(self.objectives[obj_name].evaluate([sted_image], conf1, conf2, fg_s, fg_c), obj_name)
+            for obj_name in self.objectives.keys()
+        ]))
 
     def isin(self, value, obj_name):
         """
