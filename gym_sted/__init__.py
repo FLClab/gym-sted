d8463b5cb41753aa9577e11610937c979c539e2e
Anthony <anthonybilodeau214@gmail.com>
2021-07-07 07:17:23 -0400

    moved debug env to their own folder;

--- gym_sted/__init__.py
+++ gym_sted/__init__.py
@@ -5,3 +5,9 @@ register(
     id='STED-v0',
     entry_point='gym_sted.envs:STEDEnv',
 )
+
+register(
+    id="STEDdebugResolutionSNR-v0",
+    entry_point='gym_sted.envs:DebugResolutionSNRSTEDEnv',
+    max_episode_steps=3
+)
