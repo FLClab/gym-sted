dcd48da38b0f7e4149e217b49d097cef9146df6d
Benoit Turcotte <benoiturcotte@gmail.com>
2021-07-15 15:52:24 -0400

    finna try to bleach stuff in a timed exp

--- gym_sted/envs/debug_env.py
+++ gym_sted/envs/debug_env.py
@@ -2,6 +2,7 @@ import gym
 import numpy
 import random
 
+import pysted.base
 from gym import error, spaces, utils
 from gym.utils import seeding
 from matplotlib import pyplot
@@ -260,7 +261,7 @@ class DebugBleachSTEDTimedEnv(gym.Env):
 
     obj_names = ["Resolution", "Bleach", "SNR"]
 
-    def __init__(self):
+    def __init__(self, time_quantum_us=1, exp_time_us=500000):
 
         self.synapse_generator = SynapseGenerator(mode="mushroom", seed=42)
         self.microscope_generator = MicroscopeGenerator()
@@ -281,11 +282,63 @@ class DebugBleachSTEDTimedEnv(gym.Env):
         self.viewer = None
 
         # Do I need to init other stuff since this is a timed exp ?
+        # maybe I need an exp_time, time_quantum_us, clock ?
+        # These 2 vals will be the same for every exp (episode) and do not change during an exp so no need to be reset
+        self.time_quantum_us = time_quantum_us
+        self.exp_time_us = exp_time_us
+
+        self.clock = None
+        self.temporal_experiment = None
 
         self.seed()
 
     def step(self, action):
+
+        # We manually rescale and clip the actions which are out of action space
+        m, M = -1, 1
+        action = (action - m) / (M - m)
+        action = action * (self.action_space.high - self.action_space.low) + self.action_space.low
+        action = numpy.clip(action, self.action_space.low, self.action_space.high)
+
+        # Generates imaging parameters
+        sted_params = self.microscope_generator.generate_params(
+            imaging={
+                "pdt": 100.0e-6,
+                "p_ex": 2.0e-6,
+                "p_sted": action[0]   # for now the agent only controls the STED power
+            }
+        )
+
+        """
+        where were we
+        for the first version of my debugging exp, I will do:
+        acquire an 'instant' confocal
+            - instant meaning it doesn't affect the time of the exp / get interrupted by flash updates or w/e
+        acquire the STED
+            - with all the time management wizardry
+        acquire an 'instant' confocal
+        Then compute the difference between to two confocals to see how much I bleached to give the reward
+        """
+        conf_params = self.microscope_generator.generate_params()
+
+        # Acquire an 'instant' confocal image
+        conf1, bleached, _ = self.microscope.get_signal_and_bleach(
+            self.temporal_datamap, self.temporal_datamap.pixelsize, **conf_params, bleach=False
+        )
+
+        # Acquire a STED image (with all the time wizardy bullshit)
+        sted_image, bleached = self.temporal_experiment.play_action(**sted_params)
+
+        # Acquire an 'instant' confocal image
+        conf2, bleached, _ = self.microscope.get_signal_and_bleach(
+            self.temporal_datamap, self.temporal_datamap.pixelsize, **conf_params, bleach=False
+        )
+
+        # do stuff to compute the rewards and stuff here
         pass
+
+        # return something eventually :)
+        return None
 
     def reset(self):
         """
@@ -302,7 +355,27 @@ class DebugBleachSTEDTimedEnv(gym.Env):
 
         # a temporal_datamap has been created with its flash_tstack, so we are ready to start an experiment loop?
         # what else am I missing?
-        pass
+        # tous les ptits gars qui étaient à None dans le init doivent avoir une valeur mtn si je comprends bien
+        # pas certain de comment je veux gérer le init state vu que prendre une img prend du temps, je veux tu
+        # assumer qu'une confocale est fait et qu'ensuite l'exp est lancée? ou qu'une exp commence toujours avec
+        # une confocal avec des params fix que l'agent ne choisit pas donc chaque exp a toujours une qté de temps
+        # bouffée au début? hmm cas 1 est plus simple for sure mais idk :) je vais assumer cas 1 for now :)
+        # Acquire confocal image which sets the current state
+        conf_params = self.microscope_generator.generate_params()
+        self.state, _, _ = self.microscope.get_signal_and_bleach(
+            self.temporal_datamap, self.temporal_datamap.pixelsize, **conf_params, bleach=False
+        )
+
+        # comment est-ce que je veux calculer mon bleaching dans le cas où la datamap flash dans le temps?
+        self.initial_count = self.temporal_datamap.base_datamap.sum()
+
+        # reset the other params specific to the timed exp paradigm
+        self.clock = pysted.base.Clock(self.time_quantum_us)
+        self.temporal_experiment = pysted.base.TemporalExperiment(self.clock, self.microscope, self.temporal_datamap,
+                                                                  self.exp_time_us, bleach=True)
+
+        # that's it ? ... ???
+        return self.state[numpy.newaxis, ...]
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
