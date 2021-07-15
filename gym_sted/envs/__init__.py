55a745fc9319e23c42f24f6e75b5d779cda1b85f
Benoit Turcotte <benoiturcotte@gmail.com>
2021-07-15 16:02:02 -0400

    fixed the init so the exp can launch :)

--- gym_sted/envs/__init__.py
+++ gym_sted/envs/__init__.py
@@ -1,3 +1,3 @@
 
 from gym_sted.envs.sted_env import STEDEnv
-from gym_sted.envs.debug_env import DebugResolutionSNRSTEDEnv, DebugBleachSTEDEnv
+from gym_sted.envs.debug_env import DebugResolutionSNRSTEDEnv, DebugBleachSTEDEnv, DebugBleachSTEDTimedEnv
