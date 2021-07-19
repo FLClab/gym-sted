68fd1113048bb608d5af29e12ac9f1d90f1c021f
Anthony Bilodeau <anthonybilodeau214@gmail.com>
2021-07-19 10:31:37 -0400

    added a multiply reward calculation;

--- gym_sted/rewards/rewards.py
+++ gym_sted/rewards/rewards.py
@@ -77,9 +77,47 @@ class SumRewardCalculator(RewardCalculator):
         """
         return sum([
             1 - self.rescale(self.objectives[obj_name].evaluate([sted_image], conf1, conf2, fg_s, fg_c), obj_name)
-            if obj_name != "SNR" else self.rescale(self.objectives[obj_name].evaluate([sted_image], conf1, conf2, fg_s, fg_c), obj_name) 
+            if obj_name != "SNR" else self.rescale(self.objectives[obj_name].evaluate([sted_image], conf1, conf2, fg_s, fg_c), obj_name)
             for obj_name in self.objectives.keys()
         ])
+
+    def rescale(self, value, obj_name):
+        """
+        Rescales the reward to be within approximately [0, 1] range
+
+        :param value: The value of the objective
+        :param obj_name: The name of the objective
+        """
+        return (value - self.scales[obj_name]["min"]) / (self.scales[obj_name]["max"] - self.scales[obj_name]["min"])
+
+class MultiplyRewardCalculator(RewardCalculator):
+    def __init__(self, objs, *args, **kwargs):
+        """
+        Instantiates the `RewardCalculator`
+
+        :param objs: A `dict` of objective
+        """
+        super().__init__(objs)
+        self.scales = kwargs.get("scales")
+
+    def evaluate(self, sted_image, conf1, conf2, fg_s, fg_c):
+        """
+        Evaluates the objectives and returns whether all objectives are within
+        the predefined bounds
+
+        :param sted_stack: A `numpy.ndarray` of the acquired STED image
+        :param conf1: A `numpy.ndarray` of the acquired confocal image before
+        :param conf2: A `numpy.ndarray` of the acquired confocal image after
+        :param fg_s: A `numpy.ndarray` of the STED foreground
+        :param fg_c: A `numpy.ndarray` of the confocal foreground
+
+        :returns : A `list` of rewards
+        """
+        return numpy.prod([
+            1 - self.rescale(self.objectives[obj_name].evaluate([sted_image], conf1, conf2, fg_s, fg_c), obj_name)
+            if obj_name != "SNR" else self.rescale(self.objectives[obj_name].evaluate([sted_image], conf1, conf2, fg_s, fg_c), obj_name)
+            for obj_name in self.objectives.keys()
+        ]).item()
 
     def rescale(self, value, obj_name):
         """
