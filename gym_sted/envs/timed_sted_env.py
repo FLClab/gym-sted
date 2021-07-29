2695c26c13888fe6ad58bf2562bf5a7469df5afe
Benoit Turcotte <benoiturcotte@gmail.com>
2021-07-29 13:46:36 -0400

    fixed? a bug where an acq would launch with insufficient time to image a single pixel, causing an error and crashing training

--- gym_sted/envs/timed_sted_env.py
+++ gym_sted/envs/timed_sted_env.py
@@ -117,58 +117,101 @@ class timedExpSTEDEnv(gym.Env):
                 for name in ["pdt", "p_ex", "p_sted"]
             }
         )
-        conf_params = self.microscope_generator.generate_params()
+        if sted_params["pdt"][0, 0] + self.temporal_experiment.clock.current_time >= self.exp_time_us:
+            # case where the agent doesnt have the time to image a single pixel given the time left in the experiment
+            # and the selected pdt
 
-        # Acquire confocal image
-        conf1, bleached, _ = self.microscope.get_signal_and_bleach(
-            self.temporal_datamap, self.temporal_datamap.pixelsize, **conf_params, bleach=False
-        )
+            # set the sted_image, conf1, fg_c and fg_s to a bunch of zeros,
+            sted_image = numpy.zeros(self.dmap_shape)
+            conf1 = numpy.zeros(self.dmap_shape)
+            fg_c = numpy.zeros(self.dmap_shape)
+            fg_s = numpy.zeros(self.dmap_shape)
+            n_molecs_init = self.temporal_datamap.base_datamap.sum(),
+            n_molecs_post = self.temporal_datamap.base_datamap.sum()
 
-        n_molecs_init = self.temporal_datamap.base_datamap.sum()
+            # this is the scalarized reward
+            reward = self.reward_calculator.evaluate(sted_image, conf1, fg_s, fg_c, n_molecs_init, n_molecs_post,
+                                                     self.temporal_datamap)
+            # this is the vector of individual rewards for individual objectives
+            rewards = self._reward_calculator.evaluate(sted_image, conf1, fg_s, fg_c, n_molecs_init, n_molecs_post,
+                                                       self.temporal_datamap)
 
-        # Acquire a STED image (with all the time wizardy bullshit)
-        sted_image, bleached = self.temporal_experiment.play_action(**sted_params)
+            done = True
+            info = {
+                "action": action,
+                "bleached": None,   # not sure what to do here, will this cause problems? hope not :)
+                "sted_image": sted_image,
+                "conf1": conf1,
+                "fg_c": fg_c,
+                "fg_s": fg_s,
+                "rewards": rewards,
+                "pdt": action[self.actions.index("pdt")],
+                "p_ex": action[self.actions.index("p_ex")],
+                "p_sted": action[self.actions.index("p_sted")]
+            }
 
-        n_molecs_post = self.temporal_datamap.base_datamap.sum()
+            # faut que j'update mon state avec ma plus récente acq :)
+            self.state.enqueue(sted_image)
+            observation = numpy.transpose(self.state.to_array(), (1, 2, 0))
 
-        # foreground on confocal image
-        fg_c = get_foreground(conf1)
-        # foreground on sted image
-        if numpy.any(sted_image):
-            fg_s = get_foreground(sted_image)
+            # ~!* RETURN DLA SCRAP ICITTE *!~
+            return observation, reward, done, info
+
         else:
-            fg_s = numpy.ones_like(fg_c)
-        # remove STED foreground points not in confocal foreground, if any
-        fg_s *= fg_c
+            # case where the agent can image at least one pixel given the time left in the exp and selected pdt
 
-        # this is the scalarized reward
-        reward = self.reward_calculator.evaluate(sted_image, conf1, fg_s, fg_c, n_molecs_init, n_molecs_post,
-                                                 self.temporal_datamap)
-        # this is the vector of individual rewards for individual objectives
-        rewards = self._reward_calculator.evaluate(sted_image, conf1, fg_s, fg_c, n_molecs_init, n_molecs_post,
-                                                   self.temporal_datamap)
+            conf_params = self.microscope_generator.generate_params()
 
-        done = self.temporal_experiment.clock.current_time >= self.exp_time_us
+            # Acquire confocal image
+            conf1, bleached, _ = self.microscope.get_signal_and_bleach(
+                self.temporal_datamap, self.temporal_datamap.pixelsize, **conf_params, bleach=False
+            )
 
-        info = {
-            "action": action,
-            "bleached": bleached,
-            "sted_image": sted_image,
-            "conf1": conf1,
-            "fg_c": fg_c,
-            "fg_s": fg_s,
-            "rewards": rewards,
-            "pdt": action[self.actions.index("pdt")],
-            "p_ex": action[self.actions.index("p_ex")],
-            "p_sted": action[self.actions.index("p_sted")]
-        }
+            n_molecs_init = self.temporal_datamap.base_datamap.sum()
 
-        # faut que j'update mon state avec ma plus récente acq :)
-        self.state.enqueue(sted_image)
-        observation = numpy.transpose(self.state.to_array(), (1, 2, 0))
+            # Acquire a STED image (with all the time wizardy bullshit)
+            sted_image, bleached = self.temporal_experiment.play_action(**sted_params)
 
-        # ~!* RETURN DLA SCRAP ICITTE *!~
-        return observation, reward, done, info
+            n_molecs_post = self.temporal_datamap.base_datamap.sum()
+
+            # foreground on confocal image
+            fg_c = get_foreground(conf1)
+            # foreground on sted image
+            if numpy.any(sted_image):
+                fg_s = get_foreground(sted_image)
+            else:
+                fg_s = numpy.ones_like(fg_c)
+            # remove STED foreground points not in confocal foreground, if any
+            fg_s *= fg_c
+
+            # this is the scalarized reward
+            reward = self.reward_calculator.evaluate(sted_image, conf1, fg_s, fg_c, n_molecs_init, n_molecs_post,
+                                                     self.temporal_datamap)
+            # this is the vector of individual rewards for individual objectives
+            rewards = self._reward_calculator.evaluate(sted_image, conf1, fg_s, fg_c, n_molecs_init, n_molecs_post,
+                                                       self.temporal_datamap)
+
+            done = self.temporal_experiment.clock.current_time >= self.exp_time_us
+
+            info = {
+                "action": action,
+                "bleached": bleached,
+                "sted_image": sted_image,
+                "conf1": conf1,
+                "fg_c": fg_c,
+                "fg_s": fg_s,
+                "rewards": rewards,
+                "pdt": action[self.actions.index("pdt")],
+                "p_ex": action[self.actions.index("p_ex")],
+                "p_sted": action[self.actions.index("p_sted")]
+            }
+
+            # faut que j'update mon state avec ma plus récente acq :)
+            self.state.enqueue(sted_image)
+            observation = numpy.transpose(self.state.to_array(), (1, 2, 0))
+
+            # ~!* RETURN DLA SCRAP ICITTE *!~
+            return observation, reward, done, info
 
     def reset(self):
         synapse = self.synapse_generator.generate()
