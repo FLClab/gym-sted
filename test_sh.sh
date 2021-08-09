b1b6ab991068ba5b875acec73ebcda42b91186eb
Benoit Turcotte <benoiturcotte@gmail.com>
2021-08-09 12:36:55 -0400

    trying to use a sh file to run stuff on calcul canada? idk

---
+++ test_sh.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Useful documentation: https://docs.google.com/document/d/1-a0vWkz7x5JNSlDRdf5EW2wRLviC4F__g4S4YGexHZA/edit#
+#
+#SBATCH --time=24:00:00
+#SBATCH --account=def-flavielc
+#SBATCH --cpus-per-task=26
+#SBATCH --mem=30G
+#SBATCH --array=0-4
+#SBATCH --gres=gpu:v100:1
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --mail-user=anbil106@ulaval.ca
+#SBATCH --mail-type=ALL
+#
+
+#### PARAMETERS
+
+# Use this directory venv, reusable across RUNs
+VENV_DIR=${HOME}/projects/def-adurand/hazeless/neurips/gym_sted_env
+
+module load python/3.8
+
+[ -d $VENV_DIR ] || virtualenv --no-download $VENV_DIR
+
+source $VENV_DIR/bin/activate
+
+#### RUNNING STUFF
+
+# Moves to working folder
+cd ~/projects/def-adurand/hazeless/neurips/gym_sted/gym-sted-pfrl
+
+echo "**** STARTED TRAINING ****"
+
+python main.py --env gym_sted:STEDtimed-v2 --outdir ./data/test2 --steps 10 --eval-interval 10 --eval-n-runs 2
+
+echo "**** ENDED TRAINING ****"
