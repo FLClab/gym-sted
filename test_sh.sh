5aa7fa1b8da9a372469c7e2ceba65194b6ebd4dd
Benoit Turcotte <benoiturcotte@gmail.com>
2021-08-10 16:08:56 -0400

    modified launch script for calcul canada

--- test_sh.sh
+++ test_sh.sh
@@ -2,11 +2,11 @@
 #
 # Useful documentation: https://docs.google.com/document/d/1-a0vWkz7x5JNSlDRdf5EW2wRLviC4F__g4S4YGexHZA/edit#
 #
-#SBATCH --time=00:20:00
+#SBATCH --time=48:00:00
 #SBATCH --account=def-adurand
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=30G
-#SBATCH --array=0-4
+#SBATCH --array=0-1
 #SBATCH --gres=gpu:v100:1
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --mail-user=betur57@ulaval.ca
@@ -31,6 +31,6 @@ cd ~/projects/def-adurand/hazeless/neurips/gym_sted
 
 echo "**** STARTED TRAINING ****"
 
-python gym-sted-pfrl/main.py --env gym_sted:STEDtimed-v2 --outdir ./data/test2 --steps 10 --eval-interval 10 --eval-n-runs 2
+python gym-sted-pfrl/main.py --env gym_sted:STEDtimed-v3 --outdir ./data/bump_env_training_1
 
 echo "**** ENDED TRAINING ****"
