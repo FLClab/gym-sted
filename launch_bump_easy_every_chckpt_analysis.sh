1996c2f0025b8fccf12d954b9f332a69f0f30d64
Benoit Turcotte <benoiturcotte@gmail.com>
2021-08-24 14:43:09 -0400

    added launch scripts for results analysis

---
+++ launch_bump_easy_every_chckpt_analysis.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Useful documentation: https://docs.google.com/document/d/1-a0vWkz7x5JNSlDRdf5EW2wRLviC4F__g4S4YGexHZA/edit#
+#
+#SBATCH --time=48:00:00
+#SBATCH --account=def-adurand
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=30G
+#SBATCH --array=0
+#SBATCH --gres=gpu:v100:1
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --mail-user=betur57@ulaval.ca
+#SBATCH --mail-type=ALL
+#
+
+#### PARAMETERS
+
+# Use this directory venv, reusable across RUNs
+VENV_DIR=${HOME}/projects/def-adurand/hazeless/neurips/gym_sted_env
+
+module load python/3.8
+
+[ -d $VENV_DIR ] || virtualenv --no-download $VENV_DIR
+
+source $VENV_DIR/bin/activate
+
+#### RUNNING STUFF
+
+# Moves to working folder
+cd ~/projects/def-adurand/hazeless/neurips/gym_sted
+
+echo "**** STARTED TRAINING ****"
+
+python gym-sted-pfrl/analysis/gym2_results_analysis_1.py --main_dir ./data/bump_env_training_v4_easy/20210820-092418_d345a88e/ --analysis_n_runs 50 --checkpoints_interval 1000
+
+echo "**** ENDED TRAINING ****"
