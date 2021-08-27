34ba7dbcaa99e3c999f349f4db2b2bc1bdf44fb9
Anthony <anthonybilodeau214@gmail.com>
2021-08-27 06:28:46 -0400

    modified gym1 to give reward at each step based on rank;

--- gym_sted/envs/ranking_sted_env.py
+++ gym_sted/envs/ranking_sted_env.py
@@ -386,11 +386,17 @@ class rankSTEDMultiObjectivesEnv(gym.Env):
         if (main_action == 0) or (main_action == 1 and self.num_request_left <= 0):
             # Acquire an image with the given parameters
             sted_image, bleached, conf1, conf2, fg_s, fg_c = self._acquire(imaging_action)
+            mo_objs = self.mo_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c)
+
+            # Reward is proportionnal to the ranked position of the last image
+            articulation, sorted_indices = self.preference_articulation.articulate(
+                self.episode_memory["mo_objs"] + [mo_objs]
+            )
+            index = numpy.argmax(sorted_indices).item()
+            # Reward is given by the position in the sorting
+            reward = (index + 1) / len(sorted_indices)
 
             done = False
-            reward = 0
-            mo_objs = self.mo_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c)
-            # nb_obj = self.nb_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c, synapse=self.synapse)
 
         elif main_action == 1:
             # Asking for preference
@@ -401,7 +407,6 @@ class rankSTEDMultiObjectivesEnv(gym.Env):
             fg_s = numpy.zeros(self.observation_space[0].shape[:-1])
             fg_c = numpy.zeros(self.observation_space[0].shape[:-1])
 
-            done = False
             reward = 1
             mo_objs = [scales_dict[obj_name]["max"] if obj_name != "SNR" else scales_dict[obj_name]["min"] for obj_name in self.obj_names]
             self.num_request_left -= 1
@@ -409,21 +414,14 @@ class rankSTEDMultiObjectivesEnv(gym.Env):
             if len(self.episode_memory["mo_objs"]) > 0:
                 self.current_articulation, _ = self.preference_articulation.articulate(self.episode_memory["mo_objs"])
 
+            done = False
+
         elif main_action == 2:
             # Acquire final image
             sted_image, bleached, conf1, conf2, fg_s, fg_c = self._acquire(imaging_action)
             mo_objs = self.mo_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c)
-            nb_obj = self.nb_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c, synapse=self.synapse)
 
-            # Reward is proportionnal to the ranked position of the last image
-            articulation, sorted_indices = self.preference_articulation.articulate(
-                self.episode_memory["mo_objs"] + [mo_objs]
-            )
-            index = numpy.argmax(sorted_indices).item()
-            # Reward is given by the position in the sorting
-            reward = (index + 1) / len(sorted_indices)
-            if self.scale_rank_reward:
-                reward = reward * nb_obj
+            reward = self.nb_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c, synapse=self.synapse)
 
             done = True
 
@@ -679,11 +677,17 @@ class rankSTEDRecurrentMultiObjectivesEnv(gym.Env):
         if (main_action == 0) or (main_action == 1 and self.num_request_left <= 0):
             # Acquire an image with the given parameters
             sted_image, bleached, conf1, conf2, fg_s, fg_c = self._acquire(imaging_action)
+            mo_objs = self.mo_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c)
+
+            # Reward is proportionnal to the ranked position of the last image
+            articulation, sorted_indices = self.preference_articulation.articulate(
+                self.episode_memory["mo_objs"] + [mo_objs]
+            )
+            index = numpy.argmax(sorted_indices).item()
+            # Reward is given by the position in the sorting
+            reward = (index + 1) / len(sorted_indices)
 
             done = False
-            reward = 0
-            mo_objs = self.mo_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c)
-            # nb_obj = self.nb_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c, synapse=self.synapse)
 
         elif main_action == 1:
             # Asking for preference
@@ -694,7 +698,6 @@ class rankSTEDRecurrentMultiObjectivesEnv(gym.Env):
             fg_s = numpy.zeros(self.observation_space[0].shape[:-1])
             fg_c = numpy.zeros(self.observation_space[0].shape[:-1])
 
-            done = False
             reward = 1
             mo_objs = [scales_dict[obj_name]["max"] if obj_name != "SNR" else scales_dict[obj_name]["min"] for obj_name in self.obj_names]
             self.num_request_left -= 1
@@ -702,21 +705,14 @@ class rankSTEDRecurrentMultiObjectivesEnv(gym.Env):
             if len(self.episode_memory["mo_objs"]) > 0:
                 self.current_articulation, _ = self.preference_articulation.articulate(self.episode_memory["mo_objs"])
 
+            done = False
+
         elif main_action == 2:
             # Acquire final image
             sted_image, bleached, conf1, conf2, fg_s, fg_c = self._acquire(imaging_action)
             mo_objs = self.mo_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c)
-            nb_obj = self.nb_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c, synapse=self.synapse)
 
-            # Reward is proportionnal to the ranked position of the last image
-            articulation, sorted_indices = self.preference_articulation.articulate(
-                self.episode_memory["mo_objs"] + [mo_objs]
-            )
-            index = numpy.argmax(sorted_indices).item()
-            # Reward is given by the position in the sorting
-            reward = (index + 1) / len(sorted_indices)
-            if self.scale_rank_reward:
-                reward = reward * nb_obj
+            reward = self.nb_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c, synapse=self.synapse)
 
             done = True
 
@@ -883,7 +879,7 @@ if __name__ == "__main__":
             obs, reward, done, info = env.step(env.action_space.sample())
             # print(obs[1])
             # obs, reward, done, info = env.step(numpy.array(i))
-            # print(reward, info["mo_objs"], int(info["action"][-1]))
+            print(reward, info["mo_objs"], int(info["action"][-1]))
 
             if done:
                 print(f"Episode {i} done.\n")
