d8a7ecbaea963bbd4dabafc674c2589514996f97
Benoit Turcotte <benoiturcotte@gmail.com>
2021-09-29 09:59:15 -0400

    modified reward computation to try and fit 2d gaussians on the identified points. If the FWHM of a fitted gaussian is bigger than the diffraction limit (>200nm) then the point is rejected as a guess

--- gym_sted/rewards/objectives_timed.py
+++ gym_sted/rewards/objectives_timed.py
@@ -11,13 +11,74 @@ only objective used to compute rewards. The other objectives will be fed to the 
 ***
 """
 
-from abc import ABC, abstractmethod
-
 import numpy
 import warnings
-
-from skimage.feature import peak_local_max
 import metrics
+
+from abc import ABC, abstractmethod
+from skimage.feature import peak_local_max
+from scipy import optimize
+
+
+def gaussian(height, center_x, center_y, width_x, width_y):
+    """Returns a gaussian function with the given parameters"""
+    width_x = float(width_x)
+    width_y = float(width_y)
+    return lambda x,y: height*numpy.exp(
+                -(((center_x-x)/width_x)**2+((center_y-y)/width_y)**2)/2)
+
+
+def moments(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution by calculating its
+    moments """
+    total = data.sum()
+    X, Y = numpy.indices(data.shape)
+    x = (X*data).sum()/total
+    y = (Y*data).sum()/total
+    col = data[:, int(y)]
+    width_x = numpy.sqrt(numpy.abs((numpy.arange(col.size)-x)**2*col).sum()/col.sum())
+    row = data[int(x), :]
+    width_y = numpy.sqrt(numpy.abs((numpy.arange(row.size)-y)**2*row).sum()/row.sum())
+    height = data.max()
+    return height, x, y, width_x, width_y
+
+
+def fitgaussian(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution found by a fit"""
+    params = moments(data)
+    errorfunction = lambda p: numpy.ravel(gaussian(*p)(*numpy.indices(data.shape)) -
+                                 data)
+    p, success = optimize.leastsq(errorfunction, params)
+    return p
+
+
+def find_nanodomains(acquired_signal, pixelsize, window_half_size=3, min_distance=2, threshold_rel=0.5):
+    # first step is to find peaks in the image
+    guess_positions = peak_local_max(acquired_signal, min_distance=min_distance, threshold_rel=threshold_rel)
+
+    padded_signal = numpy.pad(acquired_signal, window_half_size, constant_values=0)
+
+    # then we try to fit a gaussian on every identified peak. If one of its sigma is > 200 nm, we reject the guess
+    valid_guesses = []
+    for row, col in guess_positions:
+        #TODO : what if the ND is on the edge of the image? I should test this :)
+        data = padded_signal[(row + window_half_size) - window_half_size:
+                             (row + window_half_size) + window_half_size + 1,
+                             (col + window_half_size) - window_half_size:
+                             (col + window_half_size) + window_half_size + 1]
+
+        params = fitgaussian(data)
+        # fwhm is approx 2.355 * sigma, sigma_x is params[3], sigma_y is params[4]
+        fwhm_x, fwhm_y = 2.355 * params[3] * pixelsize, 2.355 * params[4] * pixelsize
+        if fwhm_x > 200e-9 or fwhm_y > 200e-9:
+            pass
+        else:
+            valid_guesses.append((row, col))
+    valid_guesses = numpy.asarray(valid_guesses)
+
+    return valid_guesses
 
 
 class Objective(ABC):
@@ -316,7 +377,8 @@ class NumberNanodomains(Objective):
         Identify local maxima to 'guess' a number of nanodomains
         """
         gt_coords = numpy.asarray(temporal_datamap.synapse.nanodomains_coords)
-        guess_coords = peak_local_max(sted_stack, min_distance=threshold, threshold_rel=0.5)
+        # guess_coords = peak_local_max(sted_stack, min_distance=threshold, threshold_rel=0.5)
+        guess_coords = find_nanodomains(sted_stack, temporal_datamap.pixelsize)
 
         """
         problème potentiel :
@@ -328,9 +390,14 @@ class NumberNanodomains(Objective):
         --> Quand même important à garder en tête, pourrais peut-être causer des comportements indésirés, mais pour 
             l'instant je vais garder cette implem
         """
-        if temporal_datamap.nanodomains_active_currently:
-            detector = metrics.CentroidDetectionError(gt_coords, guess_coords, threshold, algorithm="hungarian")
-            f1_score = detector.f1_score
-        else:
-            f1_score = 0
+        # should I keep this condition? I feel like this is the kind of thing we talked about on monday :/
+        # if temporal_datamap.nanodomains_active_currently:
+        #     detector = metrics.CentroidDetectionError(gt_coords, guess_coords, threshold, algorithm="hungarian")
+        #     f1_score = detector.f1_score
+        # else:
+        #     f1_score = 0
+
+        detector = metrics.CentroidDetectionError(gt_coords, guess_coords, threshold, algorithm="hungarian")
+        f1_score = detector.f1_score
+
         return f1_score
