cca0ec36eb23cc09babab7d30c5152537db05e96
Anthony <anthonybilodeau214@gmail.com>
2023-03-15 12:26:49 -0400

    fixed names in Tests

--- tests/TestMakeEnv.py
+++ tests/TestMakeEnv.py
@@ -8,10 +8,10 @@ env = gym.make("gym_sted:ContextualMOSTED-easy-lshb-v0")
 
 env = gym.make("gym_sted:ContextualMOSTED-hard-v0")
 
-env = gym.make("gym_sted:SequencelMOSTED-easy-hslb-v0")
-env = gym.make("gym_sted:SequencelMOSTED-easy-hshb-v0")
-env = gym.make("gym_sted:SequencelMOSTED-easy-lslb-v0")
-env = gym.make("gym_sted:SequencelMOSTED-easy-lshb-v0")
+env = gym.make("gym_sted:SequenceMOSTED-easy-hslb-v0")
+env = gym.make("gym_sted:SequenceMOSTED-easy-hshb-v0")
+env = gym.make("gym_sted:SequenceMOSTED-easy-lslb-v0")
+env = gym.make("gym_sted:SequenceMOSTED-easy-lshb-v0")
 
 env = gym.make("gym_sted:SequenceMOSTED-hard-v0")
 
