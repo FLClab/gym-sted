71b85d21e18251b9a92cc5fc3bff2b55336f35c3
Anthony <anthonybilodeau214@gmail.com>
2023-03-15 12:23:18 -0400

    forgot issue and upadated make env;

--- gym_sted/prefnet/__init__.py
+++ gym_sted/prefnet/__init__.py
@@ -1,6 +1,2 @@
 
-<<<<<<< HEAD
-from .articulation import load_demonstrations
-=======
 from .articulation import PreferenceArticulator, load_demonstrations
->>>>>>> 825f7744ebfccfe813f42d3725fce7994deb5bf6
--- tests/TestMakeEnv.py
+++ tests/TestMakeEnv.py
@@ -1,6 +1,18 @@
 
 import gym
 
-env = gym.make("gym_sted:STEDdebug-v0")
+env = gym.make("gym_sted:ContextualMOSTED-easy-hslb-v0")
+env = gym.make("gym_sted:ContextualMOSTED-easy-hshb-v0")
+env = gym.make("gym_sted:ContextualMOSTED-easy-lslb-v0")
+env = gym.make("gym_sted:ContextualMOSTED-easy-lshb-v0")
+
+env = gym.make("gym_sted:ContextualMOSTED-hard-v0")
+
+env = gym.make("gym_sted:SequencelMOSTED-easy-hslb-v0")
+env = gym.make("gym_sted:SequencelMOSTED-easy-hshb-v0")
+env = gym.make("gym_sted:SequencelMOSTED-easy-lslb-v0")
+env = gym.make("gym_sted:SequencelMOSTED-easy-lshb-v0")
+
+env = gym.make("gym_sted:SequenceMOSTED-hard-v0")
 
 print("done with success")
