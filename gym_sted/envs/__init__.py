2741d893809c8527e20277ac1b18f1366a2ce362
Anthony Bilodeau <anthonybilodeau214@gmail.com>
2023-05-15 10:28:12 -0400

    fixed init;

--- gym_sted/envs/__init__.py
+++ gym_sted/envs/__init__.py
@@ -1,4 +1,6 @@
 
+import logging
+
 from gym_sted.envs.sted_env import STEDEnv, STEDEnvWithoutVision, STEDEnvWithDelayedReward
 from gym_sted.envs.ranking_sted_env import (
     rankSTEDSingleObjectiveEnv,
@@ -26,9 +28,15 @@ from gym_sted.envs.preference_sted_env import (
     PreferenceSTEDMultiObjectivesEnv,
     PreferenceCountRateScaleSTEDMultiObjectivesEnv
 )
-from gym_sted.envs.abberior_env import (
-    AbberiorSTEDMultiObjectivesEnv
-)
+try:
+    from gym_sted.envs.abberior_env import (
+        AbberiorSTEDMultiObjectivesEnv
+    )
+except ModuleNotFoundError as e:
+    logging.warn(e)
+    logging.warn("Not all required packages are installed for Abberior envs...")
+    logging.warn("Using it will raise errors!")    
+    
 from gym_sted.envs.timed_sted_env import timedExpSTEDEnv, timedExpSTEDEnvBleach
 from gym_sted.envs.timed_sted_env_debug import timedExpSTEDEnvDebug
 from gym_sted.envs.pre_traj_debugging_env import preTrajDebugEnv, preTrajDebugEnvLin, preTrajDebugEnvExp
