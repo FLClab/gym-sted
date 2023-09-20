5e75749d86aeb63e005744c1383853da63eac5d9
Anthony <anthonybilodeau214@gmail.com>
2023-09-20 16:31:41 -0400

    create preference env recurrent;

--- gym_sted/envs/preference_sted_env.py
+++ gym_sted/envs/preference_sted_env.py
@@ -286,3 +286,112 @@ class PreferenceCountRateScaleSTEDMultiObjectivesEnv(PreferenceSTEDMultiObjectiv
         self.state = numpy.stack((state, conf1, sted_image), axis=-1)
 
         return (self.state.astype(numpy.uint16), obs.astype(numpy.float32)), reward, done, False, info        
+    
+class RecurrentPreferenceCountRateScaleSTEDMultiObjectivesEnv(PreferenceSTEDMultiObjectivesEnv):
+    """
+    Creates a `PreferenceCountRateScaleSTEDMultiObjectivesEnv`. In this case the 
+    reward is still calculated using the `PrefNet` model with a sigmoid activation 
+    to normalize the scores within [0, 1]. We give negative feedback when the model 
+    selects actions that leads to >1MHz photons in the acquired STED image
+
+    Action space
+        The action space corresponds to the imaging parameters
+
+    Observation space
+        The observation space is a tuple, where
+        1. The current confocal image
+        2. A vector containing the current articulation, the selected actions, the obtained objectives
+
+    """
+    metadata = {'render.modes': ['human']}
+    obj_names = ["Resolution", "Bleach", "SNR"]
+
+    def __init__(self, bleach_sampling="constant", actions=["p_sted"],
+                    max_episode_steps=30, scale_nanodomain_reward=1.,
+                    normalize_observations=True, max_count_rate=20e+6,
+                    negative_reward=-10.):
+
+        super(RecurrentPreferenceCountRateScaleSTEDMultiObjectivesEnv, self).__init__(
+            bleach_sampling = bleach_sampling,
+            actions = actions,
+            max_episode_steps = max_episode_steps,
+            scale_nanodomain_reward = scale_nanodomain_reward,
+            normalize_observations=normalize_observations,
+            max_count_rate=max_count_rate
+        )
+
+        self.negative_reward = negative_reward
+
+        # Update observation space
+        self.observation_space = spaces.Tuple((
+            spaces.Box(0, 2**16, shape=(96, 96, 3), dtype=numpy.uint16),
+            spaces.Box(
+                0, 1, shape=(1 + 1 + len(self.obj_names) + len(self.actions),),
+                dtype=numpy.float32
+            ) # Articulation, shape is given by objectives, actions at each steps
+        ))                
+
+    def step(self, action):
+
+        # Action is an array of size self.actions and main_action
+        # main action should be in the [0, 1, 2]
+        # We manually clip the actions which are out of action space
+        action = numpy.clip(action, self.action_space.low, self.action_space.high)
+
+        # Acquire an image with the given parameters
+        sted_image, bleached, conf1, conf2, fg_s, fg_c = self._acquire(action)
+        mo_objs = self.mo_reward_calculator.evaluate(sted_image, conf1, conf2, fg_s, fg_c)
+
+        # Reward is given by the objectives
+        reward, _, _ = self.preference_articulation.articulate(
+            [mo_objs], use_sigmoid=False
+        )
+        reward = reward.item()
+
+        # Reward will be dependant on the count rate of the STED image
+        # We extract the pixel dwelltime used to acquire
+        sted_params = self.microscope_generator.generate_params(
+            imaging = {
+                name : action[self.actions.index(name)]
+                    if name in self.actions else getattr(defaults, name.upper())
+                    for name in ["pdt", "p_ex", "p_sted"]
+            }
+        )
+        count_rate = sted_image.max() / sted_params["pdt"]
+        if count_rate > self.max_count_rate:
+            reward = self.negative_reward
+
+        # Updates memory
+        done = self.current_step >= self.spec.max_episode_steps - 1
+        self.current_step += 1
+        self.episode_memory["mo_objs"].append(mo_objs)
+        self.episode_memory["actions"].append(action)
+        self.episode_memory["reward"].append(reward)
+
+        info = {
+            "action" : action,
+            "bleached" : bleached,
+            "sted_image" : sted_image,
+            "conf1" : conf1,
+            "conf2" : conf2,
+            "fg_c" : fg_c,
+            "fg_s" : fg_s,
+            "mo_objs" : mo_objs,
+            "reward" : reward,
+            "sample" : self.group,
+            "conf_params" : self.conf_params
+        }
+
+        conf_params = self.microscope_generator.generate_params()
+        # Build the observation space
+        obs = [
+            self.conf_params["p_ex"] / conf_params["p_ex"], 
+            count_rate / self.max_count_rate,
+            *self.action_normalizer(self.episode_memory["actions"][-1]),
+            *self.obj_normalizer(self.episode_memory["mo_objs"][-1])
+        ]
+        
+        state = self._update_datamap()
+        self.state = numpy.stack((state, conf1, sted_image), axis=-1)
+
+        return (self.state.astype(numpy.uint16), obs.astype(numpy.float32)), reward, done, False, info            
